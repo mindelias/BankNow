42a76878688929d02752909a778e0fbb433cb783
mindelias@gmail.com <mindelias@gmail.com>
2020-06-04 16:09:09 +0100

    use cors

--- server/app.js
+++ server/app.js
@@ -6,12 +6,16 @@ import logger from "morgan";
 import config from "./config";
 import * as error from "./config/error";
 import routes from "../server/routes/";
+import cors from 'cors'
 
 const app = express();
+
+
 
 if (config.env === "development") {
   app.use(logger("dev"));
 }
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
